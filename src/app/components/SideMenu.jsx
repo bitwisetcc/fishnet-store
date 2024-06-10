fb1417a69e0393b066839a16cc3a937253f37f68
Jorge Terence <jorge.terence@outlook.com>
2024-06-10 20:32:02 -0300

    added icons

--- src/app/components/SideMenu.jsx
+++ src/app/components/SideMenu.jsx
@@ -7,7 +7,7 @@ const SideMenuItems = {
   Home: "/",
   Produtos: "/products",
   Perfil: "/profile",
-  Carrinho: "/cart",
+  // Carrinho: "/cart",
 };
 
 export default function SideMenu() {
@@ -21,7 +21,20 @@ export default function SideMenu() {
             <>
               <div className="relative flex h-full">
                 <Popover.Button className="relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none hover:text-stone-300">
-                  Menu
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                  </svg>
                 </Popover.Button>
               </div>
 
@@ -41,19 +54,75 @@ export default function SideMenu() {
                       <button onClick={close}>X</button>
                     </div>
                     <ul className="flex flex-col gap-6 items-start justify-start">
-                      {Object.entries(SideMenuItems).map(([name, href]) => {
-                        return (
-                          <li key={name}>
-                            <a
-                              href={href}
-                              className="text-3xl leading-10 text-stone-300 hover:text-stone-400 transition-colors duration-300"
-                              onClick={close}
-                            >
-                              {name}
-                            </a>
-                          </li>
-                        );
-                      })}
+                      <li>
+                        <a
+                          href="/"
+                          className="text-3xl leading-10 text-stone-300 hover:text-stone-400 transition-colors duration-300 flex gap-3 items-center *:stroke-2"
+                          onClick={close}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                            />
+                          </svg>
+                          <span>Home</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/products"
+                          className="text-3xl leading-10 text-stone-300 hover:text-stone-400 transition-colors duration-300 flex gap-3 items-center *:stroke-2"
+                          onClick={close}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                            />
+                          </svg>
+                          <span>Produtos</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="/profile"
+                          className="text-3xl leading-10 text-stone-300 hover:text-stone-400 transition-colors duration-300 flex gap-3 items-center *:stroke-2"
+                          onClick={close}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                            />
+                          </svg>
+                          <span>Perfil</span>
+                        </a>
+                      </li>
                     </ul>
                     <div className="flex flex-col gap-y-6">
                       <div
