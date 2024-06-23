70edb4f0c829b5bd85a0d7c9ca074fbb3b789caf
Giovanni Cicero <92113467+Walrus01@users.noreply.github.com>
2024-06-23 12:26:09 -0300

    fixed merging bugs

--- src/app/(app)/cart/page.js
+++ src/app/(app)/cart/page.js
@@ -146,7 +146,6 @@ function CartItem({ cartItem, onRemove }) {
           width={150}
           height={100}
           className="rounded-lg shadow-sm w-20 md:w-36"
-          priority
         />
         <div>
           <h3 className="text-sm md:text-base">{cartItem.name}</h3>
@@ -173,7 +172,7 @@ function CartItem({ cartItem, onRemove }) {
               />
             </svg>
           </button>
-          <span>{prod.quantity}</span>
+          <span>{cartItem.quantity}</span>
         </div>
       </td>
       <td className="text-end">{price(cartItem.price * cartItem.quantity)}</td>
