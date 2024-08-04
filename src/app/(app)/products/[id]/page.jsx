71d07f89fec31c065cebce082a6f8e0730806104
Jorge Terence <jorge.terence@outlook.com>
2024-08-03 22:41:02 -0300

    added maximized image dialog

--- src/app/(app)/products/[id]/page.jsx
+++ src/app/(app)/products/[id]/page.jsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { addCartItem, ensureCart } from "@/app/lib/cart";
-import { price } from "@/app/lib/format";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { ShoppingCartIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { addCartItem, ensureCart } from "@/app/lib/cart";
 import { getProductById } from "@/app/lib/query";
+import { price } from "@/app/lib/format";
 import Counter from "@/app/components/Counter";
 import Options from "@/app/components/Options";
 
@@ -13,18 +15,15 @@ export default () => {
   const { id } = useParams();
   const [prod, setProd] = useState(undefined);
   const [loading, setLoading] = useState(true);
+  const [imgDialogOpen, setImgDialogOpen] = useState(false);
 
   useEffect(() => {
     getProductById(id)
-      .then((p) => {
-        setProd(p);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+      .then((p) => setProd(p))
+      .finally(() => setLoading(false));
   }, [id]);
 
-  if (loading) {
+  if (loading)
     return (
       <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-gray-200">
         <img
@@ -35,23 +34,26 @@ export default () => {
         />
       </div>
     );
-  }
 
-  if (!prod) {
-    return <h1>Produto não encontrado</h1>;
-  }
+  if (!prod) return <h1>Produto não encontrado</h1>;
 
   return (
-    <section className="h-full grid-cols-3 place-content-center gap-16 p-8 pr-12 lg:grid">
-      <ProductOverview prod={prod} />
+    <section className="h-full grid-cols-4 place-content-center gap-16 p-8 pr-12 lg:grid">
+      <ImageDialog
+        prod={prod}
+        open={imgDialogOpen}
+        setOpen={setImgDialogOpen}
+      />
       <Image
         src={prod.img}
         alt={prod.name}
         width={500}
         height={500}
-        className="mt-2 rounded-lg border border-stone-500 shadow-lg shadow-stone-300"
+        className="col-span-2 mt-2 cursor-zoom-in rounded-lg border border-stone-500 shadow-lg shadow-stone-300"
+        onClick={() => setImgDialogOpen(true)}
         priority
       />
+      <ProductOverview prod={prod} />
       <ProductOptions prod={prod} />
     </section>
   );
@@ -92,6 +94,8 @@ function ProductOptions({ prod }) {
 
   let [done, setDone] = useState(false);
 
+  const hasWarning = (p) => p.feeding.toLowerCase().includes("nivor");
+
   function addToCart() {
     ensureCart();
     addCartItem(prod.id, size, quantity);
@@ -125,8 +129,7 @@ function ProductOptions({ prod }) {
           <hr className="my-5" />
         </>
       )}
-      {(prod.feeding.toLowerCase().includes("omnivor") ||
-        prod.feeding.toLowerCase().includes("carnivor")) && (
+      {hasWarning(prod) && (
         <>
           <div className="rounded-lg border border-yellow-500 bg-yellow-100 p-4">
             <p className="mb-2 font-semibold text-yellow-700">Atenção</p>
@@ -146,23 +149,39 @@ function ProductOptions({ prod }) {
         Adicionar ao carrinho
       </button>
       <a href="/cart">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className={`ml-3 inline size-6 transition-opacity duration-500 ${
-            done ? "opacity-80" : "opacity-0"
-          }`}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-          />
-        </svg>
+        <ShoppingCartIcon
+          className={`ml-3 inline size-6 transition-opacity duration-500 ${done ? "opacity-80" : "opacity-0"}`}
+        />
       </a>
     </article>
+  );
+}
+
+function ImageDialog({ prod, open, setOpen }) {
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      className="relative z-20"
+    >
+      <div className="fixed inset-0 flex w-screen items-center justify-center bg-slate-800/80 pt-10">
+        <DialogPanel className="relative">
+          <Image
+            src={prod.img}
+            alt={prod.name}
+            width={500}
+            height={500}
+            className="cursor-zoom-out shadow-lg"
+            onClick={() => setOpen(false)}
+          />
+          <button
+            onClick={() => setOpen(false)}
+            className="absolute right-2 top-2 rounded-full bg-black p-1 text-white"
+          >
+            <XMarkIcon className="size-5" />
+          </button>
+        </DialogPanel>
+      </div>
+    </Dialog>
   );
 }
