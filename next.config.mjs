035a179dd8a3500b6dc43fef8f820516da4e78a7
Jorge Terence <jorge.terence@outlook.com>
2024-10-21 21:17:56 -0300

    added new sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "aquaticarts.com", "fishnet-api-py.onrender.com", "pixfeeds.com","cdn.shopify.com"],
-  }
+    remotePatterns: [
+      "images.unsplash.com",
+      "aquaticarts.com",
+      "fishnet-api-py.onrender.com",
+      "pixfeeds.com",
+      "cdn.shopify.com",
+      "github.com",
+      "github.githubassets.com",
+      "f.i.bol.com.br"
+    ],
+  },
 };
 
 export default nextConfig;
