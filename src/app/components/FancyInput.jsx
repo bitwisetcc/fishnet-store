6866cd269b138a5a3999b9e13665d6f1e6d087ea
Jorge Terence <jorge.terence@outlook.com>
2024-11-03 10:19:44 -0300

    improved checkout form fields

--- src/app/components/FancyInput.jsx
+++ src/app/components/FancyInput.jsx
@@ -1,6 +1,6 @@
-export default ({ label, required, ...props }) => (
+export default ({ label, required, name, htmlAccept = "text" }) => (
   <div className="field">
-    <input className="peer" type="text" required={required} placeholder=" " />
+    <input className="peer" name={name} type={htmlAccept} required={required} placeholder=" " />
     <label className="peer-focus:top-1 peer-focus:text-xs peer-[:not(:placeholder-shown)]:top-1 peer-[:not(:placeholder-shown)]:text-xs">
       {label}
     </label>
